060508c924b2f2d6d9ec8f0d73df9d8e3b3bf400
Charbak <charbak@gmail.com>
2026-04-01 12:48:49 +0530

    Fix navigation references - remove non-existent navigation IDs

--- app/src/main/java/com/anc/ruralhealth/ui/MainActivity.kt
+++ app/src/main/java/com/anc/ruralhealth/ui/MainActivity.kt
@@ -26,9 +26,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
-                R.id.navigation_pregnancies,
-                R.id.navigation_dashboard,
-                R.id.navigation_settings
+                R.id.navigation_dashboard
             )
         )
         
